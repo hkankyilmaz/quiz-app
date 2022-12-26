492d66ad8b6cb17dcb9949813b20767475e565d3
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2022-12-27 01:09:07 +0300

    xyz

--- index.js
+++ index.js
@@ -47,12 +47,21 @@ class Quiz {
   }
 
   startQuiz(divid, buttonid1, buttonid2, buttonid3) {
-    this._questions.map((question) => {
-      async () => {
-        await setTimeout(() => {
-          document.getElementById(divid).innerHTML = question[0];
+    const qs = () => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          // document.getElementById(divid).innerHTML = question[0];
+          // document.getElementById(buttonid1).innerHTML = question[1].a;
+          // document.getElementById(buttonid2).innerHTML = question[1].b;
+          // document.getElementById(buttonid3).innerHTML = question[1].c;
+          resolve(console.log("deneme"));
         }, 5000);
-      };
+      });
+    };
+    console.log(this._questions);
+    this._questions._questions.map(async (question, idx) => {
+      await qs(question);
+      await qs(question);
     });
   }
 }
@@ -61,23 +70,26 @@ let questions = new Question();
 
 questions.addQuestions(
   "Asagilardan Hnagisi Turkiyenin Baskentidir ?",
-  [({ a: "Yozgat" }, { b: "Batman" }, { c: "Ankara" })],
+  { a: "Yozgat", b: "Batman", c: "Ankara" },
   "Ankara"
 );
 questions.addQuestions(
   "Iskandinav Mitolojinde Thorun Cekicinin Adi Nedir ?",
-  [({ a: "Mjollnir" }, { b: "Mjolnir" }, { c: "Mjolnirr" })],
+  { a: "Mjollnir", b: "Mjolnir", c: "Mjolnirr" },
   "Ankara"
 );
 questions.addQuestions(
   "Counter Strike Globel Oyununda AWP Silahi Sarjor Kapasitesi Nedir ? ",
-  [({ a: "7" }, { b: "5" }, { c: "31" })],
+  { a: "7", b: "5", c: "31" },
   "5"
 );
 questions.addQuestions(
   "2022 Turkiye Survivor Yarismasini Kim Kazanmistir",
-  [({ a: "Nisa" }, { b: "Osman" }, { c: "Messi" })],
+  [{ a: "Nisa", b: "Osman", c: "Messi" }],
   "Nisa"
 );
 
-console.log(question1, questions);
+console.log(questions);
+
+let quiz = new Quiz(questions);
+quiz.startQuiz();
