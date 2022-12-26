b023ec04d04c0348d304ffefd4bf5f2ecc10184e
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2022-12-26 15:00:32 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+class Questions {
+  constructor(question, options, answer) {
+    question = this.question;
+    options = this.options;
+    answer = this.options;
+  }
+
+  get;
+}
+
+class Quiz {
+  constructor(questions) {
+    this.questions = questions;
+  }
+}
