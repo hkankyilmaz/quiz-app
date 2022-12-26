aab55730bb731d4b3f3e258d22e63fc949b3abf1
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2022-12-26 19:40:18 +0300

    constructers created

--- index.js
+++ index.js
@@ -1,15 +1,83 @@
-class Questions {
+class Question {
   constructor(question, options, answer) {
-    question = this.question;
-    options = this.options;
-    answer = this.options;
+    this._question = question;
+    this._options = options;
+    this._answer = answer;
+    this._questions = [];
   }
 
-  get;
+  get question() {
+    return this._question;
+  }
+
+  set question(value) {
+    this._question = value;
+  }
+
+  get options() {
+    return this._options;
+  }
+  set options(value) {
+    this._options = value;
+  }
+
+  get answer() {
+    return this._answer;
+  }
+  set answer(value) {
+    this._answer = value;
+  }
+
+  addQuestions(question, options, answer) {
+    let arr = [question, options, answer];
+    this._questions.push(arr);
+  }
 }
 
 class Quiz {
   constructor(questions) {
-    this.questions = questions;
+    this._questions = questions;
+  }
+
+  get question() {
+    return this._questions;
+  }
+  set question(value) {
+    this._questions = value;
+  }
+
+  startQuiz(divid, buttonid1, buttonid2, buttonid3) {
+    this._questions.map((question) => {
+      async () => {
+        await setTimeout(() => {
+          document.getElementById(divid).innerHTML = question[0];
+        }, 5000);
+      };
+    });
   }
 }
+
+let questions = new Question();
+
+questions.addQuestions(
+  "Asagilardan Hnagisi Turkiyenin Baskentidir ?",
+  [({ a: "Yozgat" }, { b: "Batman" }, { c: "Ankara" })],
+  "Ankara"
+);
+questions.addQuestions(
+  "Iskandinav Mitolojinde Thorun Cekicinin Adi Nedir ?",
+  [({ a: "Mjollnir" }, { b: "Mjolnir" }, { c: "Mjolnirr" })],
+  "Ankara"
+);
+questions.addQuestions(
+  "Counter Strike Globel Oyununda AWP Silahi Sarjor Kapasitesi Nedir ? ",
+  [({ a: "7" }, { b: "5" }, { c: "31" })],
+  "5"
+);
+questions.addQuestions(
+  "2022 Turkiye Survivor Yarismasini Kim Kazanmistir",
+  [({ a: "Nisa" }, { b: "Osman" }, { c: "Messi" })],
+  "Nisa"
+);
+
+console.log(question1, questions);
